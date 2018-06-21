7ed75029be2c4d888642b302514b1df0776305c8
Barakat Turki <barakat.turki@gmail.com>
2018-06-21 08:34:16 +0200

    first commit

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="container">
+    <image :style="{width: 100, height: 100}" :source="{uri: 'https://vuejs.org/images/logo.png'}"/>
+    <text class="text-color-primary size-35">Hello 100k ⭐️</text>
+    <text class="text-color-primary size-25">My First Vue Native App</text>
+  </view>
+</template>
+ 
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: #64b587;
+}
+.size-35 {
+  font-size: 35px;
+}
+.size-25 {
+  font-size: 25px;
+}
+</style>
